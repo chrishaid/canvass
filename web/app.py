8459fa971f1c3cc4a5222abaced39745a6f48e21
Chris Haid <chaid@kippchicago.org>
2015-05-29 17:39:11 -0500

    added login protectoin

--- web/app.py
+++ web/app.py
@@ -3,10 +3,12 @@ import csv
 import datetime
 from peewee import *
 from flask import Flask
-from flask import render_template
-from flask.ext.admin import Admin
+from flask import render_template, flash, redirect, url_for, session, request
+from flask.ext.admin import Admin, BaseView
 from flask.ext.admin.contrib.peewee import ModelView
 from playhouse.csv_utils import load_csv
+from flask.ext.login import LoginManager, login_user , logout_user , current_user , login_required
+from flask_wtf import Form
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '123456790'
@@ -57,7 +59,11 @@ class Contact(BaseModel):
 	type = CharField()
 	cohort = CharField()
 	community_area = CharField()
-	rz = CharField()
+	recruiting_zone = CharField()
+
+	def __unicode__(self):
+		return  '%s, %s: %s' % (self.last_name, self.first_name, self.address)
+
 
 class User(BaseModel):
 	"""User table and class"""
@@ -68,6 +74,35 @@ class User(BaseModel):
 	active = BooleanField(default = True)
 	admin = BooleanField(default = False)
 
+	class Meta:
+		order_by = ('username',)
+
+	def __unicode__(self):
+		return self.username
+
+	def is_active(self):
+		"""True, as all users are active."""
+		return True
+
+	def get_id(self):
+		"""Return the email address to satisfy Flask-Login's requirements."""
+		return unicode(self.userid)
+
+	def is_authenticated(self):
+		"""Return True if the user is authenticated."""
+		return True
+
+	def is_active(self):
+		"""Return True if the user is authenticated."""
+		return self.active
+
+	def is_anonymous(self):
+		"""False, as anonymous users aren't supported."""
+		return False
+
+	def is_admin(self):
+		return self.admin
+
 class Comment(BaseModel):
 	"""Comment for contacts from users"""
 	user = ForeignKeyField(User, related_name='comments')
@@ -75,11 +110,34 @@ class Comment(BaseModel):
 	comment = CharField()
 	timestamp = DateTimeField(default = datetime.datetime.now)
 
+	class Meta:
+		order_by = ('timestamp', 'user',)
+
+class RecruitingZone(BaseModel):
+	"""Recruiting Zone list"""
+	id = PrimaryKeyField(unique = True)
+	recruiting_zone = CharField(unique = True)
+
+	def __unicode__(self):
+		return self.recruiting_zone 
+
+class UserRZ(BaseModel):
+	id = PrimaryKeyField(unique = True)
+	recruiting_zone = ForeignKeyField(RecruitingZone)
+	user = ForeignKeyField(User)
+
+	class Meta:
+		def __unicode__(self):
+			return self.recruiting_zone
+
+
+
+
 
 def create_tables():
 	"""Create tables in postgresql db from model and import contact adresses"""
 	db.connect()
-	db.create_tables([Contact, User, Comment])
+	db.create_tables([Contact, User, Comment, RecruitingZone, UserRZ])
 
 	# populate contact table
 	csv_path = './static/combined_addresses.csv'
@@ -93,16 +151,22 @@ def create_tables():
 					active = True,
 					admin = True)
 
-#############
-# Load Data #
-#############
+	# get dis(ticnt recruiting_zones from Contact and insurt into RecruitngZone table
+	rz_qry = (RecruitingZone
+		.insert_from(
+			fields=[RecruitingZone.recruiting_zone],
+			query = Contact.select(Contact.recruiting_zone).distinct().order_by(Contact.recruiting_zone))
+		.execute())
 
 all_contacts = Contact.select()
+
+# Need to add my_contacts for logged in user
 
 ############
 # App     ##
 ############
 @app.route("/")
+@login_required
 def index():
     return render_template('index.html', 
            object_list=all_contacts
@@ -112,19 +176,92 @@ def index():
 ############
 # Admin   ##
 ############
-#def UserAdmin(ModelView):
-#	inline_models = (User,)
+
+class MyModelView(ModelView):
+	"""Extends ModelView to use Flask-Login"""
+	def is_accessible(self):
+		if not current_user.is_active() or not current_user.is_authenticated():
+			return False
+
+		if current_user.is_admin():
+			return True
+
+		return False
+
+	def _handle_view(self, name, **kwargs):
+		""" Overide built-in _handl_view to redirect users when view not accessible"""
+		if not self.is_accessible():
+			if current_user.is_authenticated():
+			# permission denied
+				abort(403)
+			else:
+				# login
+				return redirect(url_for('login', next=request.url))
+
+
+
+#def CommentsAdmin(ModelView):
+#	inline_models = (Contact)
+
 
 admin = Admin(app)
-admin.add_view(ModelView(User))
-admin.add_view(ModelView(Contact))
+admin.add_view(MyModelView(User))
+admin.add_view(MyModelView(RecruitingZone))
+admin.add_view(MyModelView(UserRZ))
+admin.add_view(MyModelView(Contact))
+admin.add_view(MyModelView(Comment))
 
 
-#@app.route('/<number>/')
-#def detail(number):
-#    return render_template('detail.html',
-#        object=csv_dict[number],
-#    )
+############
+# Login   ##
+############
+
+
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(userid):
+    return User.get(userid=userid)
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    username = request.form['username']
+    password = request.form['password']
+    registered_user = None
+    try:
+    	registered_user = User.get(User.username == username, User.password == password)
+    except:
+    	registered_user = None
+    
+    if registered_user is None:
+        flash('Username or Password is invalid' , 'error')
+        return redirect(url_for('login'))
+    
+    else:
+    	login_user(registered_user)
+    
+    	flash('Logged in successfully')
+    
+    	return redirect(request.args.get("next") or url_for('index'))
+    
+    
+
+
+
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect('login')
+
 
 if __name__ == '__main__':
 	try:
